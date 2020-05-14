aee6c46709bb6aaa636e70cb57455768fa6eb4d2
ValRobichaux <63821347+ValRobichaux@users.noreply.github.com>
2020-05-14 15:47:40 -0500

    Create KidsWithMaxCandies.cpp

---
+++ LEETCode Solutions/KidsWithMaxCandies.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int max_candies = 0;
+        vector<bool> ans;
+        for(int i = 0; i < candies.size(); i++){
+            max_candies = *max_element(candies.begin(),candies.end());
+        }
+        for(int i = 0; i < candies.size(); i++){
+            if(candies[i] + extraCandies >= max_candies){
+                 ans.push_back(true);
+            }
+            else
+                 ans.push_back(false);
+        }
+        return ans;
+    }
+};
